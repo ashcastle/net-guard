f6439979e05685e8da21cd38731090fba2d30249
ashcastle <ashcastle@users.noreply.github.com>
2026-08-16 15:18:27 +0900

    chore: standardize Homebrew Formula

--- formula/net-guard.rb
+++ formula/net-guard.rb
@@ -1,10 +1,10 @@
 class NetGuard < Formula
   desc "Automated macOS Network Security & Eavesdropping Prevention Tool"
-  homepage "https://github.com/ashcastle/netguard"
-  url "https://github.com/ashcastle/netguard/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/ashcastle/net-guard"
+  url "https://github.com/ashcastle/net-guard/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
-  head "https://github.com/ashcastle/netguard.git", branch: "main"
+  head "https://github.com/ashcastle/net-guard.git", branch: "main"
 
   depends_on :macos
   depends_on xcode: ["14.0", :build]
@@ -15,7 +15,7 @@ class NetGuard < Formula
   end
 
   service do
-    run [opt_bin/"netguard", "daemon"]
+    run [opt_bin/"netguard", "daemon", "run"]
     keep_alive true
     log_path var/"log/netguard.log"
     error_log_path var/"log/netguard.err.log"
